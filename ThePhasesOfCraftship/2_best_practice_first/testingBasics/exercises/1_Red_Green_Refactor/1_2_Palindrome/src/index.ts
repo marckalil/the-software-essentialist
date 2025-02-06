9c4cb2932a4f3a0fdf75b2fc9de87b64276b2013
Marc Kalil <marc@marckalil.com>
2025-02-06 19:48:57 +0100

    [GREEN] Was It A Rat I Saw is a palindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,7 +1,9 @@
+const whiteSpacesRegex = /\s/g;
+
 function reversedString(input: string): string {
 	return input.split('').reverse().join('');
 }
 
 export function isPalindrome(input: string): boolean {
-	return input.toLowerCase() === reversedString(input).toLowerCase();
+	return input.toLowerCase().replace(whiteSpacesRegex, '') === reversedString(input).toLowerCase().replace(whiteSpacesRegex, '');
 }
